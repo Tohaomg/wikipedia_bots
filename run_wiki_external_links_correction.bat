0f4582339e99d4b4aa819d021e66743dc3640124
MonAx <romanokurg@gmail.com>
2024-06-19 21:53:47 +0300

    Update run_wiki_external_links_correction.bat

--- run_wiki_external_links_correction.bat
+++ run_wiki_external_links_correction.bat
@@ -1,3 +1,3 @@
 ChCp 1251
-fbclid_remover.exe "en.wikipedia.org" "A" "Z" "Turned 'external' links to other Wikipedian articles into internal links"
+wiki_external_links_correction.exe "en.wikipedia.org" "A" "Z" "Turned 'external' links to other Wikipedian articles into internal links"
 @pause
